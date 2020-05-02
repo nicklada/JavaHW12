9c91fca7c95331ef60dc0b9acceab60398b925a1
lada <nicklada94@gmail.com>
2020-05-02 16:40:00 +0300

    Classes with tests created

---
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -0,0 +1,118 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+import ru.netology.repository.ProductRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductManagerTest {
+    ProductRepository repository = new ProductRepository();
+    ProductManager manager = new ProductManager(repository);
+    Book first = new Book(1,"Story1",1000,"Lada");
+    Book second = new Book(2,"Story2",2000,"Mari");
+    Book third = new Book(3,"Story3",3000,"Natali");
+    Smartphone apple = new Smartphone(4,"Iphone",10000,"Apple");
+    Smartphone nokia = new Smartphone(5,"New",10000,"Nokia");
+    Smartphone samsung = new Smartphone(6,"Galaxy",10000,"Samsung");
+    Book fourth = new Book(7,"New",3000,"Peter");
+
+   @BeforeEach
+    void setup() {
+        manager = new ProductManager(repository);
+        manager.ProductAdd(first);
+        manager.ProductAdd(second);
+        manager.ProductAdd(apple);
+        manager.ProductAdd(nokia);
+    }
+
+    @Test
+   void shouldFindBookByNameIfExists() {
+        String textToFind = "Story2";
+        Product[] expected = new Product[]{second};
+        Product[] actual = manager.searchBy(textToFind);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindBookByAuthorIfExists() {
+        String textToFind = "Lada";
+        manager.searchBy(textToFind);
+        Product[] expected = new Product[]{first};
+        Product[] actual = manager.searchBy(textToFind);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotFindBookByNameIfNotExists() {
+        String textToFind = "Story3";
+        manager.searchBy(textToFind);
+        Product[] expected = new Product[]{};
+        Product[] actual = manager.searchBy(textToFind);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotFindBookByAuthorIfNotExists() {
+        String textToFind = "Natali";
+        manager.searchBy(textToFind);
+        Product[] expected = new Product[]{};
+        Product[] actual = manager.searchBy(textToFind);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindSmartByNameIfExists() {
+        String textToFind = "Iphone";
+        manager.searchBy(textToFind);
+        Product[] expected = new Product[]{apple};
+        Product[] actual = manager.searchBy(textToFind);
+        assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldFindSmartByProducerIfExists() {
+        String textToFind = "Nokia";
+        manager.searchBy(textToFind);
+        Product[] expected = new Product[]{nokia};
+        Product[] actual = manager.searchBy(textToFind);
+        assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldNotFindSmartByNameIfNotExists() {
+        String textToFind = "Galaxy";
+        manager.searchBy(textToFind);
+        Product[] expected = new Product[]{};
+        Product[] actual = manager.searchBy(textToFind);
+        assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldNotFindSmartByProducerIfNotExists() {
+        String textToFind = "Samsung";
+        manager.searchBy(textToFind);
+        Product[] expected = new Product[]{};
+        Product[] actual = manager.searchBy(textToFind);
+        assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldFindSmartAndBookWithTheSameName() {
+        manager.ProductAdd(fourth);
+        String textToFind = "New";
+        manager.searchBy(textToFind);
+        Product[] expected = new Product[]{nokia,fourth};
+        Product[] actual = manager.searchBy(textToFind);
+        assertArrayEquals(expected, actual);
+
+    }
+
+}
